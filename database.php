2fea235a2191297d502be438543ef86c6e44c3ba
GeovanyFranco <vfrancos@miumg.edu.gt>
2020-08-25 14:56:51 -0600

    Agregando todos los archivos

---
+++ database.php
@@ -0,0 +1,24 @@
+<?php
+
+$server = 'localhost:3306';
+$username = 'root';
+$password = '';
+$database = 'colegiodb';
+
+try {
+  $conn =  mysqli_connect(
+                $server,$username,$password);
+        
+        if (!$conn) {
+            exit('Connect Error (' . mysqli_connect_errno() . ') '
+                   . mysqli_connect_error());
+        }
+        //set the default client character set 
+        mysqli_set_charset($conn, 'utf-8');
+
+        // estableciendo la BDD
+        mysqli_select_db($conn, $database);       
+} catch (PDOException $e) {
+  die('Conneccion fallida: ' . $e->getMessage());
+}
+?>
